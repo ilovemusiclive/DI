aece73aab92afb093a39165a56cf0f8183b32ac4
Joseph Murat <ilovemusiclive@gmail.com>
2017-02-08 11:52:43 +0100

    this is getting silly now SEEDS

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-categories = Catergory.create(caterogry_id: 1, name: 'Halle')
-categories = Catergory.create(caterogry_id: 2, name: 'Haus')
+Catergory.create({"caterogry_id"=>1, "name"=>'Halle'})
+Catergory.create({"caterogry_id"=>2, "name"=>'Haus'})
