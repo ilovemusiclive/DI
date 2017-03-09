b46d37462c809d188928332e33bded707b9343de
Joseph Murat <ilovemusiclive@gmail.com>
2017-03-09 13:39:41 +0100

    added paperclip default to property.rb model

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,6 +2,11 @@ class Property < ApplicationRecord
   belongs_to :admin
   belongs_to :category
 
+  Paperclip::Attachment.default_options[:url] = ':s3_amazonaws.com'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+  Paperclip::Attachment.default_options[:s3_host_name] = 'dovas-us-east-1'
+  Paperclip::Attachment.default_options[:s3_protocol] = 'https'
+
 
   has_attached_file :property_img, styles: { property_index: "250x350>", property_show: "350x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :property_img, content_type: /\Aimage\/.*\z/
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,1 @@
-Paperclip::Attachment.default_options[:s3_dovas] = 's3-eu-west-1.amazonaws.com'
+
