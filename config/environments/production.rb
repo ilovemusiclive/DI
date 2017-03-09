6661d4cab8056bd3b7308d883652d8f63c023c1c
Joseph Murat <ilovemusiclive@gmail.com>
2017-03-09 13:25:06 +0100

    and again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,6 @@ Rails.application.configure do
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
-    Paperclip::Attachment.default_options[:s3_dovas] = 's3-eu-central-1.amazonaws.com'
   }
 }
   #AWS::S3::DEFAULT_HOST.replace "s3-eu-central-1.amazonaws.com"
