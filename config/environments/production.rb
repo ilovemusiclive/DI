0e72f32cc68a54fbe1bc94cc1a8787ab6f946cee
Joseph Murat <ilovemusiclive@gmail.com>
2017-03-09 11:48:32 +0100

    do i really need to

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
+    Paperclip::Attachment.default_options[:s3_aws_amazon] = 's3-eu-central-1.amazonaws.com'
   }
 }
 
