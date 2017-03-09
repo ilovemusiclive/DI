86b3f39de8fb780720082fa2a0cdaa86f92fb7c0
Joseph Murat <ilovemusiclive@gmail.com>
2017-03-09 11:14:16 +0100

    re edited the url in production.rb AGAIN

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
 
   config.paperclip_defaults = {
-    Paperclip::Attachment.default_options[:url] = ':https://console.aws.amazon.com/s3/buckets/dovas/properties/property_imgs?region=eu-west-1'
+    Paperclip::Attachment.default_options[:s3_dovas] = 's3-eu-west-1.amazonaws.com'
     storage: :s3,
     s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
